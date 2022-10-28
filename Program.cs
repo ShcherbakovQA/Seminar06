454173b35351d65dc32f4459be3699c6b3fb0d78
Alexander <alex17.05.84@yandex.ru>
2022-10-28 19:03:39 +0300

    Task 41 done

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] Inter()
+{
+    Console.WriteLine("Введите число обозначающее количество чисел");
+    int Len = Convert.ToInt32(Console.ReadLine());
+    int[] massiv = new int[Len];
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        int temp = 0;
+        Console.WriteLine($"Введите {i + 1} число:");
+        temp = Convert.ToInt32(Console.ReadLine());
+        massiv[i] = temp;
+    }
+    return massiv;
+}
+
+void TypeMassiv(int[] arr)
+{
+    Console.WriteLine("Введены числа:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void PlusNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество положительных чисел = {count}");
+}
+int[] Numbers = Inter();
+TypeMassiv(Numbers);
+PlusNum(Numbers); 
